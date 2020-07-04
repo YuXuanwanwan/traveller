6246a75ab301176dd267295621e15139d5508256
LiYuxuan <181250076@smail.nju.edu.cn>
2020-07-04 11:13:28 +0800

    fix: 重复注册bug修复

--- src/main/java/com/example/hotel/blImpl/user/AccountServiceImpl.java
+++ src/main/java/com/example/hotel/blImpl/user/AccountServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.hotel.blImpl.user;
 
 import com.example.hotel.bl.user.AccountService;
+import com.example.hotel.data.admin.AdminMapper;
 import com.example.hotel.data.user.AccountMapper;
 import com.example.hotel.po.User;
 import com.example.hotel.util.ServiceException;
@@ -18,16 +19,23 @@ public class AccountServiceImpl implements AccountService {
     private final static String UPDATE_ERROR = "修改失败";
     @Autowired
     private AccountMapper accountMapper;
-
     @Override
     public ResponseVO registerAccount(UserVO userVO) {
         User user = new User();
         BeanUtils.copyProperties(userVO,user);
-        try {
-            accountMapper.createNewAccount(user);
-           // System.out.println(userVO.getUserType());
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+        int count=0;
+        count=accountMapper.judge_exist11(user.getEmail());
+        System.out.println(count);
+        if(count==0) {
+            try {
+                accountMapper.createNewAccount(user);
+                // System.out.println(userVO.getUserType());
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                return ResponseVO.buildFailure(ACCOUNT_EXIST);
+            }
+        }
+        else{
             return ResponseVO.buildFailure(ACCOUNT_EXIST);
         }
         return ResponseVO.buildSuccess();
